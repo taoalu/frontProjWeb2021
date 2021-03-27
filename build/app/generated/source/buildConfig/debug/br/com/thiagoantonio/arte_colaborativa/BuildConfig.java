c6faba6e73064386714da9ddb2f4440162d42c4f
Thiago Antonio <taoalu@gmail.com>
2021-03-27 19:48:05 -0300

    COMMIT

---
+++ build/app/generated/source/buildConfig/debug/br/com/thiagoantonio/arte_colaborativa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.thiagoantonio.arte_colaborativa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "br.com.thiagoantonio.arte_colaborativa";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
